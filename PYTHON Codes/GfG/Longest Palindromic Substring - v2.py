1a8acf503995916e319f071f25fc2a75ff4c0f9e
Dhiman Ghosh <dgkiitcsedual@gmail.com>
2018-05-09 18:08:06 +0530

    Add files via upload

---
+++ PYTHON Codes/GfG/Longest Palindromic Substring - v2.py
@@ -0,0 +1,52 @@
+from time import gmtime, strftime, time
+
+def chk_palindrome(word):
+    flag = True
+    if word != word[::-1]:
+        flag = False
+    return flag
+
+def longest_substring(word):
+    if chk_palindrome(word):
+        return word
+    
+    n = len(word)
+    for i in range(n-1, -1, -1):
+        f = 0
+        l = i
+        while f<=l:
+            if word[f] == word[l-1] and chk_palindrome(word[f:l]):
+                w = word[f:l]
+                if len(w) == 1:
+                    break
+                return w
+            else:
+                f+=1
+    for i in range(n):
+        f = i
+        l = n
+        while f<=l:
+            if word[f] == word[l-1] and chk_palindrome(word[f:l]):
+                w = word[f:l]
+                if len(w) == 1:
+                    return 0
+                return w
+            else:
+                l-=1
+
+def run():
+    word = input('Word: ')
+    
+    # Initial Mil_Sec
+    s = int(round(time() * 1000))
+    
+    subs = longest_substring(word)
+    if subs!=0:
+        print(subs)
+    else:
+        print('No substring possible!')
+    # Mil_Secs req 
+    print(int(round(time() * 1000)) - s)
+
+if __name__ == '__main__':
+    run()
