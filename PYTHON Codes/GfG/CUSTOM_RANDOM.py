fff33656119c625a6465f513f0803c3e8191a3ae
Dhiman Ghosh <dgkiitcsedual@gmail.com>
2018-05-24 17:32:40 +0530

    Delete CUSTOM_RANDOM.py

--- PYTHON Codes/GfG/CUSTOM_RANDOM.py
+++
@@ -1,7 +0,0 @@
-from time import gmtime, strftime, time
-
-for _ in range(10):
-    current_mil_sec = int(round(time() * 1000))
-    current_sec = int(strftime('%S', gmtime()))
-    r = (current_sec % 100) * (current_mil_sec % 1000)
-    print('Generated Random Number: '+str(r))
