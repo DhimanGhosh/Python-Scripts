2190d63891c3085f28244d800873291f6c12b081
Dhiman Ghosh <dgkiitcsedual@gmail.com>
2020-01-18 01:01:14 +0530

    Updated

--- PYTHON Codes/Patterns/Pat18.py
+++ PYTHON Codes/Patterns/Pat18.py
@@ -1,12 +1,15 @@
 '''
 5 5 5 5 5
-4 4 4 4
-3 3 3
-2 2
-1
+  4 4 4 4
+    3 3 3
+      2 2
+        1
 '''
 
+s = 0
 for i in range(1, 6)[::-1]:
+    print(' '*s, end='')
     for j in range(0, i):
         print(i, end=' ')
+    s += 2
     print()
---
+++ PYTHON Codes/Patterns/Pat19.py
@@ -0,0 +1,15 @@
+'''
+1 2 3 4 5
+  1 2 3 4
+    1 2 3
+      1 2
+        1
+'''
+
+s = 0
+for i in range(1, 6)[::-1]:
+    print(' '*s, end='')
+    for j in range(1, i+1):
+        print(j, end=' ')
+    s += 2
+    print()
